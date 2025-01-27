f3cbab63adea7b53b92ecb20194ee3b774087419
root <root@ip-10-120-0-136.ap-south-1.compute.internal>
2025-01-27 15:29:30 +0000

    logout

--- components/LogoutButton.tsx
+++ components/LogoutButton.tsx
@@ -1,23 +1,24 @@
-// filepath: components/LogoutButton.tsx
 import { Auth } from 'aws-amplify';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const LogoutButton: React.FC = () => {
-    const router = useRouter();
-  
-    const handleLogout = async () => {
-      try {
-        await Auth.signOut();
-        router.push('/'); // Redirect to home page after logout
-      } catch (error) {
-        console.error('Error signing out: ', error);
-      }
-    };
-  
-    return (
-      <button onClick={handleLogout}>
-        Logout
-      </button>
-    );
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    try {
+      await Auth.signOut();
+      router.push('/'); // Redirect to home page after logout
+    } catch (error) {
+      console.error('Error signing out: ', error);
+    }
   };
+
+  return (
+    <button onClick={handleLogout}>
+      Logout
+    </button>
+  );
+};
+
+export default LogoutButton;
