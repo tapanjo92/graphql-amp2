f26f960fe6a2f88ffecd81ab505a76afdd7cf2dd
root <root@ip-10-120-0-136.ap-south-1.compute.internal>
2025-01-28 09:27:24 +0000

    passowrd req

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,23 +1,25 @@
-
 import { defineAuth, secret } from "@aws-amplify/backend";
 
 export const auth = defineAuth({
   loginWith: {
-    // Configure password requirements
-    email: true,
-    password: {
-      minLength: 8,
-      requireNumbers: true,
-      requireSpecialCharacters: true,
-      requireUppercase: true,
-      requireLowercase: true
+    email: {
+      verification: {
+        type: "VerificationEmailWithLink",
+      },
+      password: {
+        minLength: 8,
+        requireNumbers: true,
+        requireSpecialCharacters: true,
+        requireUppercase: true,
+        requireLowercase: true,
+      },
     },
     externalProviders: {
       callbackUrls: [
-        "http://localhost:3000/"  // only localhost for initial deployment
+        "http://localhost:3000/", // only localhost for initial deployment
       ],
       logoutUrls: [
-        "http://localhost:3000/"  // only localhost for initial deployment
+        "http://localhost:3000/", // only localhost for initial deployment
       ],
       google: {
         clientId: secret("GOOGLE_CLIENT_ID"),
@@ -26,10 +28,10 @@ export const auth = defineAuth({
         attributeMapping: {
           email: "email",
           givenName: "given_name",
-          familyName: "family_name"
-        }
-      }
-    }
+          familyName: "family_name",
+        },
+      },
+    },
   },
   userAttributes: {
     email: {
@@ -43,8 +45,6 @@ export const auth = defineAuth({
     familyName: {
       required: false,
       mutable: true,
-    }
-  }
+    },
+  },
 });
-
-
