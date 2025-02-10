7af6d69acf9e073a2b28d3ab58729a8bcc3269dd
root <root@ip-10-120-0-136.ap-south-1.compute.internal>
2025-02-10 14:10:12 +0000

    adding graphql

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,71 +1,74 @@
 import { defineData } from '@aws-amplify/backend';
 
-export const schema = {
-    Comment: {
-        model: {
-            primaryKey: {
-                id: 'id',
-            },
-        },
-        fields: {
-            id: {
-                type: 'ID',
-            },
-            post: {
-                type: 'ID',
-                isRequired: true,
-            },
-            author: {
-                type: 'ID',
-                isRequired: true,
-            },
-            content: {
-                type: 'String',
-                isRequired: true,
-            },
-            createdAt: {
-                type: 'AWSDateTime',
-                isRequired: true,
-            },
-            updatedAt: {
-                type: 'AWSDateTime',
-                isRequired: true,
-            },
-        },
+const models = {
+  Comment: {
+    model: {
+      primaryKey: {
+        id: 'id',
+      },
     },
-    Post: {
-        model: {
-            primaryKey: {
-                id: 'id',
-            },
-        },
-        fields: {
-            id: {
-                type: 'ID',
-            },
-            author: {
-                type: 'ID',
-                isRequired: true,
-            },
-            title: {
-                type: 'String',
-                isRequired: true,
-            },
-            content: {
-                type: 'String',
-            },
-            createdAt: {
-                type: 'AWSDateTime',
-                isRequired: true,
-            },
-            updatedAt: {
-                type: 'AWSDateTime',
-                isRequired: true,
-            },
-        },
+    fields: {
+      id: {
+        type: 'ID',
+      },
+      post: {
+        type: 'ID',
+        isRequired: true,
+      },
+      author: {
+        type: 'ID',
+        isRequired: true,
+      },
+      content: {
+        type: 'String',
+        isRequired: true,
+      },
+      createdAt: {
+        type: 'AWSDateTime',
+        isRequired: true,
+      },
+      updatedAt: {
+        type: 'AWSDateTime',
+        isRequired: true,
+      },
     },
+  },
+  Post: {
+    model: {
+      primaryKey: {
+        id: 'id',
+      },
+    },
+    fields: {
+      id: {
+        type: 'ID',
+      },
+      author: {
+        type: 'ID',
+        isRequired: true,
+      },
+      title: {
+        type: 'String',
+        isRequired: true,
+      },
+      content: {
+        type: 'String',
+      },
+      createdAt: {
+        type: 'AWSDateTime',
+        isRequired: true,
+      },
+      updatedAt: {
+        type: 'AWSDateTime',
+        isRequired: true,
+      },
+    },
+  },
 };
 
-export const data = defineData({
-    schema,
-});
+const schema = {
+  version: 1,
+  models,
+};
+
+export const data = defineData(schema);
