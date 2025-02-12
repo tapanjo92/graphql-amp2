a7476c8a11be5d66e29e8dc78142573e5de6720d
root <root@ip-10-120-0-136.ap-south-1.compute.internal>
2025-02-12 12:02:29 +0000

    adding graphql and dynamo

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,26 +1,14 @@
 import { defineData } from '@aws-amplify/backend';
-import { type Schema } from '@aws-amplify/data-schema';
-
-const schema = {
-  models: {
-    Note: {
-      primaryKey: {
-        fieldName: 'id',
-        type: 'ID',
-      },
-      fields: {
-        id: { type: 'ID', required: true },
-        content: { type: 'String', required: true },
-        createdAt: { type: 'AWSDateTime', required: true },
-        updatedAt: { type: 'AWSDateTime', required: true }
-      }
-    }
-  }
-} satisfies Schema;
 
 export const data = defineData({
-  schema,
-  authorizationModes: {
-    defaultAuthorizationMode: 'userPool'
-  }
+  schema: ({ id, string }) => ({
+    // Example model definition
+    Todo: {
+      primaryIndex: { partitionKey: id('id') },
+      fields: {
+        id: id(),
+        name: string(),
+      }
+    }
+  })
 });
